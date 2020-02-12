b365c29d0ae5bb774a765053cba43f138fe2944d
Владимирович <vldmrvch@MacBook-Vldmrvch.local>
2020-02-12 17:47:42 +0200

    test

---
+++ people/README.md
@@ -0,0 +1,9 @@
+# Pymier blog
+
+[http://pp.pymi.vn/](http://pp.pymi.vn/)
+
+Blog của các Pymier - chia sẻ kiến thức - góp sức uống bia.
+
+Blog sử dụng Pelican, làm theo hướng dẫn tại [đây](http://pymi.vn/blog/blog-markdown-pelican-githubpages/)
+
+Một sản phẩm của các Pymier, vào ổ tại [đây](http://invite.pymi.vn/)
---
+++ people/pelicanconf.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Pymiers'
+SITENAME = 'The PyMiers'
+SITEURL = ''
+THEME = 'themes/pymi'
+STATIC_PATHS = ['images', 'extra/CNAME']
+CSS_FILE = 'app.css'
+PLUGIN_PATHS = ["plugins", ]
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight', 'pygments_style': 'monokai'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Ho_Chi_Minh'
+
+DEFAULT_LANG = 'vi'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+DEFAULT_PAGINATION = 5
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+ARTICLE_URL = 'article/{slug}/'
+ARTICLE_SAVE_AS = 'article/{slug}/index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL = 'tags/'
+TAGS_SAVE_AS = 'tags/index.html'
+
+AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_URL = 'authors/'
+AUTHORS_SAVE_AS = 'authors/index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORYS_URL = 'categories/'
+CATEGORYS_SAVE_AS = 'categories/index.html'
+
+ARCHIVES_SAVE_AS = 'archives/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/{date:%m}/index.html'
+
+
+
+# Blogroll
+LINKS = (('PyMi homepage', 'https://pymi.vn/'),
+         ('FAMILUG.org', 'http://www.familug.org/'),
+         )
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/pyfml/'),
+          ('GitHub', 'https://github.com/pymivn/'),
+          )
+
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
